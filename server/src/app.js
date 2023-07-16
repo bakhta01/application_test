b0e263de4418f318fe3efb83e1b4c5f0ae290ff3
bakhta01 <bakhtafadhul@gmail.com>
2023-07-16 15:21:26 +0200

    p

--- server/src/app.js
+++ server/src/app.js
@@ -4,17 +4,14 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
-
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status', (req, res) => {
+app.post('/redister', (req, res) => {
   res.send({
-    message: 'bakhta',
-  });
-});
+    message: `hello ${req.body.email}! your user registered! have fun `
+  })
+})
 
-app.listen(process.env.PORT || 8081, () => {
-  console.log('Server is running on port 8081');
-});
+app.listen(process.env.PORT || 8081)
